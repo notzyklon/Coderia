b79c62c33bd738bf8fc27e2d95a528ef23cf0996
Zyklon <notzyklon@gmail.com>
2025-12-21 10:40:01 +0530

    Rectangular Star Pattern

---
+++ pattern8.java
@@ -0,0 +1,21 @@
+/*
+
+   ******
+   ******
+   ******
+   ******
+
+*/
+public class pattern8
+{
+    public void main(String args[])
+    {
+        int m=4,n=6;
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
