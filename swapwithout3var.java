de6fd1a5b20f953042030f5ea77507f8fac0c008
Zyklon <notzyklon@gmail.com>
2025-12-25 13:27:55 +0530

    swap two numbers without 3rd variable

---
+++ swapwithout3var.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class swapwithout3var
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        int a,b;
+        System.out.println("Enter two numbers to swap:");
+        a=in.nextInt();
+        b=in.nextInt();
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+        a=a+b; 
+        b=a-b;
+        a=a-b;
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+        in.close();
+    }
+}
